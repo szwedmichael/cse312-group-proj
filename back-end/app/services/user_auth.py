8f04e4ebe0c6f0712235abe18b87877c27260ace
Tylerrut <99150792+Tylerrut@users.noreply.github.com>
2024-03-10 10:47:41 -0400

    finished register and logout user_auth.py

--- back-end/app/services/user_auth.py
+++ back-end/app/services/user_auth.py
@@ -5,6 +5,10 @@ import hashlib
 import secrets
 
 
+CRLF="\r\n"
+nosniff="X-Content-Type-Options: nosniff"+CRLF +CRLF
+
+
 mongo_client=MongoClient("mongo")
 #Collections(so far): "credentials"
 db=mongo_client["Vacationhub"]
@@ -13,15 +17,15 @@ db=mongo_client["Vacationhub"]
 def registerUser(username, password1, password2):
     #Make sure passwords match
     if password1 != password2:
-        return forbidden()
+        return serve403("Passwords do not match")
     #arbitrarily use password1
     password=password1
     #Make sure password is valid
     if passwordNotValid(password):
-        return forbidden()
+        return serve403("Password does fit requirements")
     #Check if username already exist
     if usernameExist(username):
-        return forbidden()
+        return serve403("Username already exists")
     
     #Hash password with salt
     salt=bcrypt.gensalt()
@@ -34,11 +38,12 @@ def registerUser(username, password1, password2):
     to_insert={}
     to_insert["username"]=username
     to_insert["hashed_password"]=hashed_password
+    to_insert["salt"]=salt
     to_insert["xsfr"]=xsfr
 
     #Insert credentials
     credentials_collection.insert_one(to_insert)
-    return found()
+    return serve302()
 
 def passwordNotValid(password):
     #All passwords valid
@@ -53,10 +58,44 @@ def usernameExist(username):
     return False
 
 #TODO Add password requirements
-#TODO Add 403 forbidden and 302 Found function; can add paramters if necessary
 
 def loginUser(username, password):
     pass
 
 def logoutUser(auth_token):
-    pass
+    credentials_collection=db["credentials"]
+    #check if auth token is not None
+    if auth_token:
+        #Hash auth
+        hashed_auth=hashlib.sha256(auth_token.encode()).hexdigest()
+        #find user docuement; fix key name and auth_token
+        user=credentials_collection.find_one({"hashed_auth":hashed_auth})
+        if user:
+            username=user["username"]
+            #Unset auth in data base
+            credentials_collection.update_one({"username":username}, {'$unset': {"hashed_auth": ''}})
+            serve302()
+    serve403("Invalid Logout Request")
+
+#Forbidden Response
+def serve403(message):
+    global CRLF
+    global nosniff
+    status="HTTP/1.1 403 Forbidden" +CRLF
+    content_type="Content-Type: text/plain" +CRLF
+    body=message.encode()
+    content_lentgh="Content-Length: " + str(len(body))+CRLF
+
+    return (status + content_type + content_lentgh + nosniff).encode()+body
+
+#Found Response
+def serve302():
+    global CRLF
+    global nosniff
+    status="HTTP/1.1 302 Found" +CRLF
+    #Location to go after signing in; to be changed
+    location="Location: /"+CRLF
+
+    return (status + location + nosniff).encode()
+
+
