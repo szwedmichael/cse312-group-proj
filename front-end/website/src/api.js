c2fe78b4fd018e7c0719bd2bfb3b1a3365994108
Matthew Collopy <mscollop@buffalo.edu>
2024-03-10 13:03:50 -0400

    change url of fast api

--- front-end/website/src/api.js
+++ front-end/website/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // npm install axios
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000', // URL of the Fast API
+    baseURL: 'http://127.0.0.1:8000', // URL of the Fast API
 });
 
 export default api;
