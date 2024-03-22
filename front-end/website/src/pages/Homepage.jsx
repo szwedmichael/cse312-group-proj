05a8f66d1753140eb986879e4fecdb1f7077b2fb
Ian Moran <idmoran24@gmail.com>
2024-03-21 23:49:08 -0400

    moved the images folder, updated the current image, and added the m&m picture to the Homepage

--- front-end/website/src/pages/Homepage.jsx
+++ front-end/website/src/pages/Homepage.jsx
@@ -1,29 +1,28 @@
 import { useState } from 'react'
 import '../styles/Homepage.css'
-
-// change hrefs to correct url
+// import React from 'react';
+import vacationImage from '../images/red-m&m.png';
 
 function Homepage() {
 
   return (
-    <>
-      <div className="home-entirepage">
-        <div className="home-header">
-          <h1>Vacation Hub</h1>
-        </div>
-        <div className= "home-login">
-          <form >
-            <a href="http://localhost:5173/login" className="login-button">Login</a>  
-          </form>
-        </div>
-        <div className= "home-signup">
-          <form>
-            <a href="http://localhost:5173/signup" className="signup-button">Sign up</a>
-          </form>
-        </div>
+    <div className="home-entirepage">
+      <div className="home-header">
+        <h1>Vacation Hub</h1>
+        <img src={vacationImage} alt="Vacation Scene" className="home-image" />
       </div>
-    </>
-  )
+      <div className="home-login">
+        <form>
+          <a href="http://localhost:5173/login" className="login-button">Login</a>
+        </form>
+      </div>
+      <div className="home-signup">
+        <form>
+          <a href="http://localhost:5173/signup" className="signup-button">Sign up</a>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Homepage
+export default Homepage;
