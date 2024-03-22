885d35ec4323c35726ab89f720819ebcd6b1ffce
Matthew Collopy <mscollop@buffalo.edu>
2024-03-22 18:56:20 -0400

    added like feature with posts

--- front-end/website/src/pages/HomepageLogged.jsx
+++ front-end/website/src/pages/HomepageLogged.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 import api from '../api.js';
 import '../styles/HomepageLogged.css'
+import likeButton from '../images/like-button.png';
 
 function HomepageLogged() {
 
     const [userName, setUserName] = useState('');
     // some mock posts
     const [posts, setPosts] = useState([
-        { id: 1, username: "yolo12", location: 'Buffalo, NY', description: 'i love it here !!!!', date: '03/2024' },
-        { id: 2, username: "ohboy", location: 'New York, NY', description: 'Boston is better ;)', date: '03/2024' },
-        { id: 3, username: "sofun", location: 'Los Angeles, CA', description: 'What happend to this place', date: '03/2024' },
+        { id: 1, username: "yolo12", location: 'Buffalo, NY', description: 'i love it here !!!!', date: '03/2024', likes: 12},
+        { id: 2, username: "ohboy", location: 'New York, NY', description: 'Boston is better ;)', date: '03/2024', likes: 20 },
+        { id: 3, username: "sofun", location: 'Los Angeles, CA', description: 'What happend to this place', date: '03/2024', likes: 5 },
     ]);
 
     useEffect(() => {
@@ -42,6 +43,7 @@ function HomepageLogged() {
         fetchPosts();
     }, []);
 
+    // logout 
     const logout = async (event) => {
         event.preventDefault();
         try {
@@ -51,6 +53,33 @@ function HomepageLogged() {
             console.error('Error logging out:', error);
         }
     }
+
+    // like or unlike post
+    const likePost = async (postId) => {
+        const data = {
+            post_id: postId,
+        }
+        const updatedPosts = posts.map(post => {
+            if (post.id === postId) {
+                // updates on frontend
+                const likedByUser = !post.likedByUser;
+                const likes = likedByUser ? post.likes + 1 : post.likes - 1;
+                return { ...post, likedByUser, likes };
+            }
+            return post;
+        });
+        setPosts(updatedPosts);
+
+        // change request whether like or unlike
+        const endpoint = updatedPosts.find(post => post.id === postId).likedByUser ? '/like-post' : '/unlike-post';
+
+        // send that post request (like-post or unlike-post)
+        try {
+            await api.post(endpoint, data);
+        } catch (error) {
+            console.error('Error updating like:', error);
+        }
+    };
 
     // posts {"location": "Buffalo, NY", "description": "I went to Niagara Falls and it was awesome", "date": MM/YYYY, "xsrf":xsrf}
     return (
@@ -70,7 +99,12 @@ function HomepageLogged() {
                         <h4>{post.location}</h4>
                         <p>{post.description}</p>
                         <div className='post-date'>{post.date}</div>
+                        <div className='like-section'>
+                            <img src={likeButton} alt="Like" onClick={(event) => likePost(post.id, event)}/>
+                            <span className='like-count'>{post.likes}</span>
+                        </div>
                     </li>
+
                 ))}
             </ul>
         </div>
