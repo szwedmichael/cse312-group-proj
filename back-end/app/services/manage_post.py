6083546be09db9f50739f7a91c93a7b9a4095547
Tylerrut <99150792+Tylerrut@users.noreply.github.com>
2024-03-24 18:53:30 -0400

    Update manage_post.py

--- back-end/app/services/manage_post.py
+++ back-end/app/services/manage_post.py
@@ -16,7 +16,7 @@ class ManagePostService:
         self.post_collection = mongo_database.get_collection("posts")
 
     # body contains something like: {"location": "Buffalo, NY", "description": "I went to Niagara Falls and it was awesome", "date": MM/YYYY, "xsrf":xsrf}
-    def addPost(self,  htmlXSRF, body, authToken: str = HTTPCookie(alias="auth_token")):
+    def addPost(self,  htmlXSRF, body, authToken: str):
         # Verify user exists
         validUser = self.validUser(authToken)
         if not validUser:
@@ -51,7 +51,7 @@ class ManagePostService:
 
         return content
 
-    def likePost(self, post_id, auth_token:str = HTTPCookie(alias="auth_token")):
+    def likePost(self, post_id, auth_token:str):
         # Get post document
         post_document = self.post_collection.find_one({"id": post_id})
         # Check if it exists
@@ -81,7 +81,7 @@ class ManagePostService:
 
         return {"post_id": post_id, "likes": likes, "like_status": True}
 
-    def unlikePost(self, post_id, auth_token:str=HTTPCookie(alias="auth_token")):
+    def unlikePost(self, post_id, auth_token:str):
         # Get post document
         post_document = self.post_collection.find_one({"id": post_id})
         # Check if it exists
