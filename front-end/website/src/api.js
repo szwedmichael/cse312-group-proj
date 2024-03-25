469e523ae6c56c0745328ec54b7c48f5450dad65
Michael Szwed <szwedmichael7@gmail.com>
2024-03-24 20:57:13 -0400

    dist/ports

--- front-end/website/src/api.js
+++ front-end/website/src/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 // npm install axios
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000', // URL of the Fast API
+  baseURL: "http://0.0.0.0:8080", // URL of the Fast API
 });
 
 export default api;
--- front-end/website/src/pages/Homepage.jsx
+++ front-end/website/src/pages/Homepage.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import '../styles/Homepage.css'
-import vacationImage from '../images/red-m&m.png';
-import api from '../api';
+import { useState, useEffect } from "react";
+import "../styles/Homepage.css";
+import vacationImage from "../images/red-m&m.png";
+import api from "../api";
 
 /*
 checks if user is logged in
@@ -9,20 +9,21 @@ renders login or signup if not
 renders homepage where posts are if they are
 */
 function Homepage() {
-
   // sends get request to verify-auth to check if user is logged in
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   useEffect(() => {
     const checkAuth = async () => {
       try {
-        const response = await api.get('/verify-auth', { withCredentials: true });
+        const response = await api.get("/verify-auth", {
+          withCredentials: true,
+        });
         if (response.data.isAuthenticated) {
           setIsAuthenticated(true);
         } else {
           setIsAuthenticated(false);
         }
       } catch (error) {
-        console.error('Error verifying authentication:', error);
+        console.error("Error verifying authentication:", error);
         setIsAuthenticated(false);
       }
     };
@@ -33,27 +34,34 @@ function Homepage() {
   // renders based on requests reponse bool (yes or no)
   return (
     <div className="home-entirepage">
-      {isAuthenticated ? (<HomepageLogged />) : (
-      
-      <>
-        <div className="home-header">
-          <h1>Vacation Hub</h1>
-          <img src={vacationImage} alt="Vacation Scene" className="home-image" />
-        </div>
-        <div className="home-login">
-          <form>
-            <a href="http://127.0.0.1:8000/login" className="login-button">Login</a>
-          </form>
-        </div>
-        <div className="home-signup">
-          <form>
-            <a href="http://127.0.0.1:8000/signup" className="signup-button">Sign up</a>
-          </form>
-        </div>
-      </>
-
+      {isAuthenticated ? (
+        <HomepageLogged />
+      ) : (
+        <>
+          <div className="home-header">
+            <h1>Vacation Hub</h1>
+            <img
+              src={vacationImage}
+              alt="Vacation Scene"
+              className="home-image"
+            />
+          </div>
+          <div className="home-login">
+            <form>
+              <a href="http://0.0.0.0:8080/login" className="login-button">
+                Login
+              </a>
+            </form>
+          </div>
+          <div className="home-signup">
+            <form>
+              <a href="http://0.0.0.0:8080/signup" className="signup-button">
+                Sign up
+              </a>
+            </form>
+          </div>
+        </>
       )}
-
     </div>
   );
 }
