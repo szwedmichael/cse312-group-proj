a490a5c1ae74d173e205634bf05f5f6d6b42bcb8
Tylerrut <tjrutley@buffalo.edu>
2024-04-28 18:50:07 -0400

    fixed backend scheduling

--- back-end/app/services/manage_post.py
+++ back-end/app/services/manage_post.py
@@ -47,7 +47,7 @@ class ManagePostService:
         if ("hour" in body) and ("minute" in body):
             time=datetime.time(body.hour, body.minute)
         else:
-            time=datetime.datetime.now()
+            time=datetime.datetime.now().time()
         # If there's no file, obtain the noUpload.jpg
         mimeType = "text/plain"
         if file == None:
@@ -148,7 +148,7 @@ class ManagePostService:
 
         for post in all_post:
             info = {}
-            post_time=info["time"]
+            post_time=info["time_stamp"]
             current_time=datetime.datetime.now()
             if post_time < current_time:
                 info["username"] = post["username"]
